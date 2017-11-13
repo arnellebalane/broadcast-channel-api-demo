bcf5d9ae139a9191f572f2a680a13e30e9189300
Arnelle Balane <arnellebalane@gmail.com>
2017-11-13 23:38:23 +0800

    add feature detection

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-const channel = new BroadcastChannel('counter');
-let value = 0;
-
 const displayedValue = document.querySelector('h1');
 const upButton = document.querySelector('.up');
 const downButton = document.querySelector('.down');
 
-updateValue(0);
+if (window.BroadcastChannel) {
+    const channel = new BroadcastChannel('counter');
+    let value = 0;
 
-function updateValue(delta) {
-    value += delta;
-    displayedValue.textContent = value;
-}
+    updateValue(0);
 
-channel.onmessage = (e) => updateValue(e.data);
+    function updateValue(delta) {
+        value += delta;
+        displayedValue.textContent = value;
+    }
 
-upButton.onclick = (e) => {
-    channel.postMessage(1);
-    updateValue(1);
-};
-downButton.onclick = (e) => {
-    channel.postMessage(-1);
-    updateValue(-1);
+    channel.onmessage = (e) => updateValue(e.data);
+
+    upButton.onclick = (e) => {
+        channel.postMessage(1);
+        updateValue(1);
+    };
+    downButton.onclick = (e) => {
+        channel.postMessage(-1);
+        updateValue(-1);
+    }
+} else {
+    upButton.remove();
+    downButton.remove();
+
+    document.body.classList.add('not-supported');
+    displayedValue.classList.add('not-supported');
+    displayedValue.textContent = 'BroadcastChannel is not supported :(';
 }
