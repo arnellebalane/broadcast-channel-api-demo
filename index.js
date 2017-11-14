65b710c9e8f15f080f7e00bd3bede40ce7df5da5
Arnelle Balane <arnellebalane@gmail.com>
2017-11-14 22:17:22 +0800

    update demo app

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-const displayedValue = document.querySelector('h1');
-const upButton = document.querySelector('.up');
-const downButton = document.querySelector('.down');
-const p = document.querySelector('p');
-
-if (window.BroadcastChannel) {
-    const channel = new BroadcastChannel('counter');
-    let value = 0;
-
-    updateValue(0);
-
-    function updateValue(delta) {
-        value += delta;
-        displayedValue.textContent = value;
-    }
-
-    channel.onmessage = (e) => updateValue(e.data);
-
-    upButton.onclick = (e) => {
-        channel.postMessage(1);
-        updateValue(1);
-    };
-    downButton.onclick = (e) => {
-        channel.postMessage(-1);
-        updateValue(-1);
-    }
-} else {
-    upButton.remove();
-    downButton.remove();
-    p.remove();
-
-    document.body.classList.add('not-supported');
-    displayedValue.classList.add('not-supported');
-    displayedValue.textContent = 'BroadcastChannel is not supported :(';
+if (!window.BroadcastChannel) {
+    throw new Error('BroadcastChannel API is not supported :(');
 }
+
+const model = document.querySelector('.model');
+const channel = new BroadcastChannel('demo');
+
+channel.onmessage = e => {
+    model.style.top = e.data.y + '%';
+    model.style.left = e.data.x + '%';
+};
+
+let dragging = false;
+
+const mouse = e => {
+    const ref = 'pageX' in e ? e : e.touches[0];
+    return { x: ref.pageX, y: ref.pageY };
+};
+
+const handlePointerDown = e => dragging = true;
+const handlePointerUp = e => dragging = false;
+const handlePointerMove = e => {
+    if (!dragging) return;
+    const position = {
+        x: mouse(e).x / window.innerWidth * 100,
+        y: mouse(e).y / window.innerHeight * 100
+    };
+    channel.postMessage(position);
+    channel.onmessage({ data: position });
+};
+
+model.onpointerdown = handlePointerDown;
+model.onmousedown = handlePointerDown;
+model.ontouchstart = handlePointerDown;
+
+document.onpointerup = handlePointerUp;
+document.onmouseup = handlePointerUp;
+document.ontouchend = handlePointerUp;
+
+document.onpointermove = handlePointerMove;
+document.onmousemove = handlePointerMove;
+document.ontouchmove = handlePointerMove;
